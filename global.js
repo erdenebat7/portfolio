99efa82388fb2f5041109b6da67ae140f166d0f1
erdenebat7 <erdenekhuub1@gmail.com>
2026-04-22 20:00:25 -0700

    Added projects.js
    more

--- global.js
+++ global.js
@@ -97,3 +97,32 @@ form?.addEventListener('submit', function (event) {
 
     location.href = url;
 });
+
+export async function fetchJSON(url) {
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+    throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
+
+export function renderProjects(project, containerElement, headingLevel = 'h2') {
+  containerElement.innerHTML = '';
+  
+  for (let project of projects) {
+    const article = document.createElement('article');
+
+    article.innerHTML = `
+      <h3>${project.title}</h3>
+      <img src="${project.image}" alt="${project.title}">
+      <p>${project.description}</p>
+    `;
+
+    containerElement.appendChild(article);
+  } 
+}
