5f257292398c111c58f860c851f2d81fd7d89f40
erdenebat7 <erdenekhuub1@gmail.com>
2026-04-23 16:02:18 -0700

    minor tweak
    minor

--- global.js
+++ global.js
@@ -118,7 +118,7 @@ export function renderProjects(projects, containerElement, headingLevel = 'h2') 
     const article = document.createElement('article');
 
     article.innerHTML = `
-      <h3>${project.title}</h3>
+      <${headingLevel}>${project.title}</${headingLevel}>
       <img src="${project.image}" alt="${project.title}">
       <p>${project.description}</p>
     `;
