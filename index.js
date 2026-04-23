e4266b268e539d9edc4a81f15313390154f2496c
erdenebat7 <erdenekhuub1@gmail.com>
2026-04-23 15:38:17 -0700

    changed json
    added years to projects

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ const projectsContainer = document.querySelector('.projects');
 
 renderProjects(latestProjects, projectsContainer, 'h3');
 
-const githubData = await fetchGitHubData('giorgianicolaou');
+const githubData = await fetchGitHubData('erdenebat7');
 
 const profileStats = document.querySelector('#profile-stats');
 
 if (profileStats) {
   profileStats.innerHTML = `
         <dl>
-          <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
-          <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
-          <dt>Followers:</dt><dd>${githubData.followers}</dd>
-          <dt>Following:</dt><dd>${githubData.following}</dd>
+          <dt>Public Repos:</dt><dd>${"https://api.github.com/users/erdenebat7/repos"}</dd>
+          <dt>Public Gists:</dt><dd>${"https://api.github.com/users/erdenebat7/gists{/gist_id}"}</dd>
+          <dt>Followers:</dt><dd>${"https://api.github.com/users/erdenebat7/followers"}</dd>
+          <dt>Following:</dt><dd>${"https://api.github.com/users/erdenebat7/following{/other_user}"}</dd>
         </dl>
     `;
 }
