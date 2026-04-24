3cb1c92e5452eb360bd725c71b45d1a250db279f
erdenebat7 <erdenekhuub1@gmail.com>
2026-04-23 17:02:57 -0700

    Debugged index.js
    removed the quotation marks

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ if (profileStats) {
           <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
           <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
           <dt>Followers:</dt><dd>${githubData.followers}</dd>
-          <dt>Following:</dt><dd>${"githubData.following}"}</dd>
+          <dt>Following:</dt><dd>${githubData.following}</dd>
         </dl>
     `;
 }
